4ab6c7843d7dc8cb3c0b2d09b71a958b84e0ada7
Toan Phung <toan.phung@proteo.info>
2024-02-25 23:25:49 +0000

    update

--- src/app/log/analysis-log/analysis-log.component.ts
+++ src/app/log/analysis-log/analysis-log.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import {WebsocketService} from "../../websocket.service";
 import {DatePipe} from "@angular/common";
 
@@ -11,7 +11,9 @@ import {DatePipe} from "@angular/common";
   templateUrl: './analysis-log.component.html',
   styleUrl: './analysis-log.component.scss'
 })
-export class AnalysisLogComponent {
+export class AnalysisLogComponent implements AfterViewInit{
+  @ViewChild('logContainer') logContainer: ElementRef | undefined;
+
   logMessages: {
     log: string,
     task_id: string,
@@ -19,9 +21,29 @@ export class AnalysisLogComponent {
     timestamp: Date
   }[] = []
   constructor(private websocketService: WebsocketService) {
+
     this.websocketService.connectAnalysisLog().asObservable().subscribe((data: any) => {
       data.timestamp = new Date(data.timestamp*1000)
-      this.logMessages = [data].concat(this.logMessages)
+
+      this.logMessages.push(data)
+      if (this.logContainer) {
+        this.logContainer.nativeElement.scrollTop = this.logContainer.nativeElement.scrollHeight
+      }
     })
+  }
+
+  ngAfterViewInit() {
+    /*for (let i = 0; i < 100; i++) {
+      this.logMessages.push({
+        log: "test",
+        task_id: "test",
+        hostname: "test",
+        timestamp: new Date()
+      })
+
+    }
+    if (this.logContainer) {
+      this.logContainer.nativeElement.scrollTop = this.logContainer.nativeElement.scrollHeight
+    }*/
   }
 }
